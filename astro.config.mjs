0a5f01ac55cfdd269c680f9caea9ac8f6884ee4a
dennisramsey <dennis.ramsey@gmail.com>
2024-03-19 19:45:15 -0700

    config updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig, squooshImageService } from "astro/config";
 import robots from "astro-robots";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +11,4 @@ export default defineConfig({
     service: squooshImageService(),
   },
   integrations: [tailwind(), sitemap(), robots()],
-  output: "server",
-  adapter: vercel(),
 });
